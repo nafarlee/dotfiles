bf1a7f492ee3630afdd0e56096cd4e76305a88e0
Nicholas Farley <ohcrap524@gmail.com>
2013-09-27 01:03:47 -0400

    cleaned up the install script a bit

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-cur_dir=$(pwd)
+dotfiles=$(pwd)
 cd vim/bundle/vim-snipmate
-ln -s $cur_dir/vim/snipps snippets
-cd $cur_dir
-files=$(ls -A | grep vim)
+ln -s $dotfiles/vim/snipps snippets
+cd $dotfiles
+files=$(ls | grep vim)
 echo "Files to be linked: $files"
 cd ~
 echo "Changed directory to home"
 for file in $files; do
-	ln -s "$cur_dir/$file" ".$file"
+	ln -s "$dotfiles/$file" ".$file"
 done
 
 echo "linking completed"
+cd $dotfiles
 
