81c5bb73c88d8d5467efe2fc7417fb9cc85b4671
Nicholas Farley <nicholasafarley@gmail.com>
2018-02-11 21:48:42 -0500

    Add install steps to install and configure fish

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ dotfiles_dir=$(dirname $(realpath $0))
 mkdir -p "$HOME/.config"
 ln -s "$dotfiles_dir/nvim" "$HOME/.config/."
 ln -s "$dotfiles_dir/fish" "$HOME/.config/."
+
+chsh -s "$(which fish)"
+fish -c 'curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher'
+fish -c 'fisher z omf/theme-bobthefish'
