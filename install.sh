83e046dbb2713a3d052be81a35872da7a32bba9c
Nicholas Farley <nicholas_farley@colpal.com>
2018-02-12 09:48:57 -0500

    Fix incorrectly symlinked config paths

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 dotfiles_dir=$(dirname $(realpath $0))
 
 mkdir -p "$HOME/.config"
-ln -s "$dotfiles_dir/nvim" "$HOME/.config/."
-ln -s "$dotfiles_dir/fish" "$HOME/.config/."
+ln -s "$dotfiles_dir/nvim" "$HOME/.config/nvim"
+ln -s "$dotfiles_dir/fish" "$HOME/.config/fish"
 
 chsh -s "$(which fish)"
 fish -c 'curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher'
