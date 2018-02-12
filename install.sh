ce3cc392d3b1999f847cc3237b2626b407af1e48
Nicholas Farley <nicholas_farley@colpal.com>
2018-02-12 10:36:07 -0500

    Add step to install gitconfig

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ dotfiles_dir="$PWD"
 mkdir -p "$HOME/.config"
 ln -sh "$dotfiles_dir/nvim" "$HOME/.config/nvim"
 ln -sh "$dotfiles_dir/fish" "$HOME/.config/fish"
+ln -sh "$dotfiles_dir/gitconfig" "$HOME/.gitconfig"
 
 chsh -s "$(which fish)"
 fish -c 'curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher'
