376c7b8f481ce103cd76387d7d7223fbe641a1d7
Nicholas Farley <nicholas_farley@colpal.com>
2018-02-12 10:40:26 -0500

    Add fish aliases for quick git diffing

---
+++ fish/functions/gdd.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function gdd --description 'alias gdd git diff --color-words'
+	git diff --color-words $argv;
+end
---
+++ fish/functions/gddd.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function gddd --description 'alias gddd git diff --color-words=.'
+	git diff --color-words=. $argv;
+end
