ddbbf541485ae0ec90ce3953abf0a0c655fa1ff2
Nicholas Farley <nicholas_farley@colpal.com>
2018-02-12 10:29:05 -0500

    Prevent nested symlinks from being created

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 dotfiles_dir="$PWD"
 
 mkdir -p "$HOME/.config"
-ln -s "$dotfiles_dir/nvim" "$HOME/.config/nvim"
-ln -s "$dotfiles_dir/fish" "$HOME/.config/fish"
+ln -sh "$dotfiles_dir/nvim" "$HOME/.config/nvim"
+ln -sh "$dotfiles_dir/fish" "$HOME/.config/fish"
 
 chsh -s "$(which fish)"
 fish -c 'curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher'
