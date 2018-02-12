3eed1bdfcd4856bbaf83485038effabb4d1aa466
Nicholas Farley <nicholasafarley@gmail.com>
2018-02-11 21:39:10 -0500

    Add line to installer to symlink fish dir

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 dotfiles_dir=$(dirname $(realpath $0))
 
 mkdir -p "$HOME/.config"
-ln -s "$dotfiles_dir/nvim" "$HOME/.config/nvim"
+ln -s "$dotfiles_dir/nvim" "$HOME/.config/."
+ln -s "$dotfiles_dir/fish" "$HOME/.config/."
