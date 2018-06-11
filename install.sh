ac5ba1cfd153632e394bb2c3c96ecdf49e37f6be
Nicholas Farley <nicholasafarley@gmail.com>
2018-06-10 22:36:32 -0400

    Remove -h option from ln commands

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 dotfiles_dir="$PWD"
 
 mkdir -p "$HOME/.config"
-ln -sh "$dotfiles_dir/nvim" "$HOME/.config/nvim"
-ln -sh "$dotfiles_dir/fish" "$HOME/.config/fish"
-ln -sh "$dotfiles_dir/gitconfig" "$HOME/.gitconfig"
+ln -s "$dotfiles_dir/nvim" "$HOME/.config/nvim"
+ln -s "$dotfiles_dir/fish" "$HOME/.config/fish"
+ln -s "$dotfiles_dir/gitconfig" "$HOME/.gitconfig"
 
 chsh -s "$(which fish)"
 fish -c 'curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher'
