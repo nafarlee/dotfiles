f5c70ba61b1d09c45f779f314eb04401741a7171
Nicholas Farley <nicholasafarley@gmail.com>
2018-06-12 22:06:42 -0400

    Fix typo

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir -p "$HOME/.config"
 ln -s "$dotfiles_dir/nvim" "$HOME/.config/nvim"
 ln -s "$dotfiles_dir/fish" "$HOME/.config/fish"
 ln -s "$dotfiles_dir/gitconfig" "$HOME/.gitconfig"
-ln -s "$dotfiles_dir/tmux.cong" "$HOME/.tmux.conf"
+ln -s "$dotfiles_dir/tmux.conf" "$HOME/.tmux.conf"
 
 chsh -s "$(which fish)"
 fish -c 'curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher'
