03199f5e4b265692a4fd50fa6f0445c4e25884dd
Nicholas Farley <nicholasafarley@gmail.com>
2019-11-21 19:39:31 -0500

    Add initialization for pyenv

--- fish/config.fish
+++ fish/config.fish
@@ -7,4 +7,10 @@ if test -d $HOME/.cargo/bin
   set -x PATH $PATH $HOME/.cargo/bin
 end
 
+if test -d $HOME/.pyenv/bin
+  set -x PATH $HOME/.pyenv/bin $PATH
+  status is-interactive; and source (pyenv init -|psub)
+  status is-interactive; and source (pyenv virtualenv-init -|psub)
+end
+
 eval (starship init fish)
