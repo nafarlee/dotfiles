3bfc92e67ab5bab5cf71e11fba7435b4c4b443fa
Nicholas Farley <nicholasafarley@gmail.com>
2021-05-15 19:52:38 -0400

    Remove fisher initialization from config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,3 @@
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
 if test -d $HOME/.cargo/bin
   set -x PATH $PATH $HOME/.cargo/bin
 end
