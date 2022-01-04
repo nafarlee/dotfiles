6bde9866542a4362ef1189b4ac068a1ce6b66876
Nicholas Farley <nicholasafarley@gmail.com>
2022-01-04 15:03:59 -0500

    Depend on nvim-web-devicons explicitly

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -8,7 +8,8 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
+  use 'kyazdani42/nvim-web-devicons'
+  use 'nvim-lualine/lualine.nvim'
   use 'dense-analysis/ale'
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   use 'mhinz/vim-signify'
