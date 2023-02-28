62a01370dd8564b238aa880b54ec11b5f3e84e42
Nicholas Farley <nicholas_farley@colpal.com>
2023-02-28 14:21:13 -0500

    Add diagnostics.shellcheck to null-ls config list

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -30,6 +30,18 @@ require("lazy").setup({
       automatic_installation = true,
     },
   },
+  {
+    'jose-elias-alvarez/null-ls.nvim',
+    dependencies = {'nvim-lua/plenary.nvim'},
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.diagnostics.shellcheck,
+        },
+      })
+    end
+  },
   {'nvim-telescope/telescope.nvim', dependencies = {'nvim-lua/plenary.nvim'}},
   {
     'nvim-lualine/lualine.nvim',
