1086288d6d9a995b6e64a43e1e31c6d55da115f7
Nicholas Farley <nicholas_farley@colpal.com>
2023-02-28 16:08:53 -0500

    Configure pylsp

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -30,6 +30,7 @@ require("lazy").setup({
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.pylsp.setup({})
     end
   },
   {
