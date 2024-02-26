619e223ec5073473b319f8730dabe2b0f236af1a
Nicholas Farley <nicholasafarley@gmail.com>
2024-02-26 17:57:41 -0500

    Add light/dark aliases for switching Kitty theme

---
+++ fish/.config/fish/functions/dark.fish
@@ -0,0 +1,4 @@
+function dark --wraps='kitty +kitten themes Catppuccin-Mocha' --description 'alias dark kitty +kitten themes Catppuccin-Mocha'
+  kitty +kitten themes Catppuccin-Mocha $argv
+        
+end
---
+++ fish/.config/fish/functions/light.fish
@@ -0,0 +1,4 @@
+function light --wraps='kitty +kitten themes Catppuccin-Latte' --description 'alias light kitty +kitten themes Catppuccin-Latte'
+  kitty +kitten themes Catppuccin-Latte $argv
+        
+end
