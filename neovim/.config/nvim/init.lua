daac7dc7d1be6d0e41652a880b2b9b92b2e12267
Nicholas Farley <nicholasafarley@gmail.com>
2024-10-13 17:17:53 -0400

    Set unique colorcolumn based on language

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -170,7 +170,18 @@ vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
-vim.o.colorcolumn = '80'
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "*" },
+  callback = function(args)
+    local ft = vim.bo[args.buf].filetype
+    local colorcolumns = {
+      racket = "102",
+      scheme = "102",
+    }
+    vim.o.colorcolumn = colorcolumns[ft] or "80"
+  end
+})
 
 vim.keymap.set('n', '<Space>', '')
 vim.keymap.set("i", "jk", "<esc>")
