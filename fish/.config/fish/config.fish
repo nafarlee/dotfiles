610b5514adcfcd7d2d840c59402b56f9a73bc67e
Nicholas Farley <nicholas_farley@colpal.com>
2025-10-31 12:42:55 -0400

    Add volta to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,9 @@ if command --query nvim
   set -x EDITOR nvim
 end
 
+set -gx VOLTA_HOME "$HOME/.volta"
+
+fish_add_path "$VOLTA_HOME/bin"
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.google-cloud-sdk/bin
 fish_add_path ~/google-cloud-sdk/bin
