0732bc44ae7f6f0fa772e266571adbd60b8a03ba
Nicholas Farley <nicholasafarley@gmail.com>
2025-11-03 17:36:06 -0500

    Remove PATH modification for pyenv

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,12 +2,6 @@ if test -d $HOME/.cargo/bin
   set -x PATH $PATH $HOME/.cargo/bin
 end
 
-if test -d $HOME/.pyenv/bin
-  set -x PATH $HOME/.pyenv/bin $PATH
-  status is-interactive; and source (pyenv init -|psub)
-  status is-interactive; and source (pyenv virtualenv-init -|psub)
-end
-
 if test -d $HOME/.google-cloud-sdk/bin
   set -x PATH $PATH $HOME/.google-cloud-sdk/bin
 end
