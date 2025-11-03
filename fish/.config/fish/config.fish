c56a042acd5d781256f901cb2da7421a9b9cc279
Nicholas Farley <nicholasafarley@gmail.com>
2025-11-03 17:40:01 -0500

    Remove sourcing of google-cloud-sdk completions

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,11 +3,6 @@ fish_add_path ~/.google-cloud-sdk/bin
 fish_add_path ~/google-cloud-sdk/bin
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/.rd/bin
-
-if test -f /home/nafarlee/.cache/yay/google-cloud-sdk/pkg/google-cloud-sdk/opt/google-cloud-sdk/path.fish.inc
-  source /home/nafarlee/.cache/yay/google-cloud-sdk/pkg/google-cloud-sdk/opt/google-cloud-sdk/path.fish.inc
-end
-
 fish_add_path ~/.local/bin
 
 starship init fish | source
