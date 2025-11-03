2dbeb3ceb25c0dabfcaf3b517184afe70cdc2131
Nicholas Farley <nicholasafarley@gmail.com>
2025-11-03 17:42:44 -0500

    Remove manual dark/light switching aliases

--- fish/.config/fish/functions/dark.fish
+++
@@ -1,4 +0,0 @@
-function dark --wraps='kitty +kitten themes Catppuccin-Mocha' --description 'alias dark kitty +kitten themes Catppuccin-Mocha'
-  kitty +kitten themes Catppuccin-Mocha $argv
-        
-end
--- fish/.config/fish/functions/light.fish
+++
@@ -1,4 +0,0 @@
-function light --wraps='kitty +kitten themes Catppuccin-Latte' --description 'alias light kitty +kitten themes Catppuccin-Latte'
-  kitty +kitten themes Catppuccin-Latte $argv
-        
-end
