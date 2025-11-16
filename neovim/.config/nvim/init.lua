ba3d2d86c57eccb3fe22f7c0c706f4cdde3646bd
Nicholas Farley <nicholasafarley@gmail.com>
2025-11-16 17:37:33 -0500

    Try to enable conjure for Gerbil

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -130,6 +130,9 @@ require("lazy").setup({
       ft = {"janet"},
       init = function()
         vim.g["conjure#filetype#janet"] = "conjure.client.janet.stdio"
+        vim.g["conjure#client#scheme#stdio#command"] = "gxi"
+        vim.g["conjure#client#scheme#stdio#value_prefix_pattern"] = false
+        vim.g["conjure#client#scheme#stdio#prompt_pattern"] = "> $?"
       end,
     },
   },
