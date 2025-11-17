99024763dddd741f28f42d0579eb92e6b6ee2c86
Nicholas Farley <nicholas_farley@colpal.com>
2025-11-17 16:30:01 -0500

    Move snacks.nvim to a separate file

--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -19,33 +19,6 @@ require("lazy").setup({
   spec = {
     { import = "plugins" },
     {
-      "folke/snacks.nvim",
-      priority = 1000,
-      lazy = false,
-      opts = {
-        bigfile = { enabled = true },
-        explorer = { enabled = true },
-        indent = { enabled = true },
-        input = { enabled = true },
-        picker = { enabled = true },
-        notifier = { enabled = true },
-        quickfile = { enabled = true },
-        scope = { enabled = true },
-        statuscolumn = { enabled = true },
-        words = { enabled = true },
-      },
-      keys = {
-        { "<C-p>", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
-        { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
-        { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
-        { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
-        { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
-        { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
-        { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
-        { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
-      },
-    },
-    {
       "mason-org/mason-lspconfig.nvim",
       opts = {},
       dependencies = {
