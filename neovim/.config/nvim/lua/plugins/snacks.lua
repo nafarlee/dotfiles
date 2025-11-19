640dd85b8cff3fd4771022c9e65457229a9cc850
Nicholas Farley <nicholas_farley@colpal.com>
2025-11-19 16:06:31 -0500

    Bind Neovim <leader>p to grep

--- neovim/.config/nvim/lua/plugins/snacks.lua
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -16,6 +16,7 @@ return {{
   },
   keys = {
     { "<C-p>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+    { "<leader>p", function() Snacks.picker.grep() end, desc = "Grep" },
     { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
     { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
     { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
