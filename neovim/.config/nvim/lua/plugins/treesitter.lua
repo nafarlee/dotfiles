bfa00a2e45eaaf63f94d510ebea55dd77d6635b8
Nicholas Farley <nicholasafarley@gmail.com>
2026-02-07 00:08:12 -0500

    Fix treesitter configuration

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,10 @@
 return {{
   'nvim-treesitter/nvim-treesitter',
+  branch = "master",
+  lazy = false,
   build = ':TSUpdate',
   config = function()
-    require'nvim-treesitter.configs'.setup {
+    require('nvim-treesitter.configs').setup {
       auto_install = true,
       sync_install = false,
       highlight = { enable = true },
